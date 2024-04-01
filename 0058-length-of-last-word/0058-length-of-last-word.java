644ef49d2d9d26c0d5d593932bc7d013af071286
khushi garg <80378602+khushi1415@users.noreply.github.com>
2024-04-01 10:42:03 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (76.16%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,18 +1,11 @@
-class Solution {
+public class Solution {
     public int lengthOfLastWord(String s) {
-        
-        Map<Character, Integer> map = new HashMap<>();
-        s = s.trim();
-        int n = s.length();
-        int count =0;
-        for(int i=n-1; i>=0; i--){
-            if(!map.containsKey(' ')){
-                map.put(s.charAt(i), count++);
-            }else{
-                return count-1;
-            }
+        int count = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' ' && count == 0) continue;
+            if (s.charAt(i) == ' ' && count != 0) return count;
+            if (s.charAt(i) != ' ') count++;
         }
-
         return count;
     }
 }
